a766a090c8bd42a6ac15fc0157444509fe4967e1
RuBublik <relikr@gmail.com>
2017-04-30 20:21:16 +0300

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,24 +1,24 @@
 import base64
 
-def encode(key, clear):
+def encode(key, clear):          # key -> password, clear -> will be encrypted
     enc = []
     for i in range(len(clear)):
         key_c = key[i % len(key)]
         enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
         enc.append(enc_c)
-    return base64.b64encode("".join(enc))
+    return base64.b64encode("".join(enc))   # returns incrypted string
 
-def decode(key, enc):
+def decode(key, enc):            # key -> password, enc -> will be decrypted
     dec = []
     enc = base64.b64decode(enc)
     for i in range(len(enc)):
         key_c = key[i % len(key)]
         dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
         dec.append(dec_c)
-    return "".join(dec)
+    return "".join(dec)         # returns decrypted string
 
-key="afgd"
-string="razdvatri"
+key="afgd"                      # serves as password.
+string="razdvatri"              # the string will be encrypted and decrypted.
 ENC = encode(key, string)
 print ENC
 DEC = decode(key, ENC)
