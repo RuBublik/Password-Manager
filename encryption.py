c7297e83279a6bba6074c5d05c20a755f30efbc1
RuBublik <relikr@gmail.com>
2017-04-30 20:12:21 +0300

    Add files via upload

---
+++ encryption.py
@@ -0,0 +1,27 @@
+import base64
+
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.b64encode("".join(enc))
+
+def decode(key, enc):
+    dec = []
+    enc = base64.b64decode(enc)
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+key="afgd"
+string="razdvatri"
+ENC = encode(key, string)
+print ENC
+DEC = decode(key, ENC)
+print DEC
+
+
